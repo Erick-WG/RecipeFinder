000750680d1fdaacdb8667d13173f159c2cc50b9
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-08-16 19:30:24 +0300

    Added the effect hook to fetch data: bug(Not loading as per search request by button click)

---
+++ recipeFinder/src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import SearchBox from "./Components/SearchBox"
+
+function App (){
+
+  return (
+    <>
+    <h1>Look up great recipes.</h1>
+    <SearchBox />
+    </>
+  )
+
+}
+
+
+export default App
